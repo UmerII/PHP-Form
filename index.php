ecd894e8406db8bb3b4a50e72951670bc739edd4
UmerII <149783576+UmerII@users.noreply.github.com>
2024-05-27 14:17:30 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+include "config.php";
+session_start();
+if(!$_SESSION['email'])
+{
+    header("location:login.php");
+}
+
+if(isset($_POST['btn_logout']))
+{
+    session_destroy();
+    header("location:login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>Title</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Portfolio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index1.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="sign-up.php">Sign Up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">login</a>
+        </li>  
+      </ul>
+      
+      <form class="d-flex" method="post">
+       <li><button name="btn_logout" class="btn btn-success">Logout</button></li>
+      </form>
+      
+    </div>
+  </div>
+</nav>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,60 @@
+<?php 
+include "config.php";
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>Login-page</title>
+</head>
+<style>
+    input{
+        margin: 10px;
+    }
+</style>
+<body>
+
+<h1 style="text-align: center" class="alert alert-dark">login form</h1>
+<div class="container">
+    <form method="post">
+        <input type="email" name="email" placeholder="Enter Your Email" class="form-control">
+        <input type="password" name="password" placeholder="Enter Your Password" class="form-control">
+        <button type="submit" name="btn-login" class="btn btn-success">login</button>
+    </form>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
+</body>
+<?php 
+
+if(isset($_POST['btn-login']))
+{
+    
+    $email = $_POST['email'];   
+    $password = $_POST['password'];
+    
+   $select = " SELECT * FROM sign_up WHERE email = '$email' and password = '$password'"; 
+   
+   $result = mysqli_query($conn,$select);
+   if(mysqli_num_rows($result))
+   {
+    $_SESSION['email'] = $email;
+    header("location:index1.php");
+    
+   }
+   
+}
+
+
+
+?>
+</html>
+
+
